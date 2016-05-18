2532fa889d42e1fe48cb495bbe504defc6d534d2
Andrey Tonkiy <atonkiybrain@gmail.com>
2016-05-18 16:22:28 +0300

    updated mailchimp

--- themes/landportal/js/main.js
+++ themes/landportal/js/main.js
@@ -2,20 +2,29 @@
     Drupal.behaviors.landportal = {
       attach : function(context, settings) {
         Drupal.behaviors.landportal.initRegistrationForm(context, settings);
+        Drupal.behaviors.landportal.initSearchSuggestions(context, settings);
       },
       initRegistrationForm: function(context, settings){
         isVisibleNewsletterLanguage();
         $('.form-item-mailchimp-lists-mailchimp-land-portal-news-digest-subscribe input').bind("change click",function(){
           isVisibleNewsletterLanguage();
         });
-      }
+      },
+      initSearchSuggestions: function(context, settings){
+        $suggestions=$('.spelling-suggestions dd').html();
+        console.log($suggestions);
+        if($suggestions != undefined){
+          $('#search-form .spelling-suggestions dt').after($suggestions);
+          $('#search-form .spelling-suggestions').addClass('show');
+        }
+      },
     };
     function isVisibleNewsletterLanguage(){
       if($('#edit-mailchimp-lists-mailchimp-land-portal-news-digest-subscribe').prop("checked")){
-          $('#edit-lang-wrap').show();
+          $('.form-item-mailchimp-lists-mailchimp-land-portal-news-digest-interest-groups-1').show();
         }
         else{
-          $('#edit-lang-wrap').hide();
+          $('.form-item-mailchimp-lists-mailchimp-land-portal-news-digest-interest-groups-1').hide();
       }
     }
   })(jQuery);
