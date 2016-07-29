5f60deccd7be6e0e92ed61054ed13df4f0bff907
Anton Zuev <zuev13@mail.ru>
2016-07-29 11:40:21 +0300

    Mini change in lbvisTable

--- modules/landbook/modules/indicators/main.js
+++ modules/landbook/modules/indicators/main.js
@@ -2,7 +2,8 @@ $( window ).load(function() {
   if(typeof COUNTRY_ISO3 !== "undefined"){
     var LBT = new lbvisTable({
       vis: LBV,
-      target: '#block-indicators-indicators-block'
+      target: '#block-indicators-indicators-block',
+      path: Drupal.settings.landbook.libraryPath + '/'
     });
     LBT.init();
   }
