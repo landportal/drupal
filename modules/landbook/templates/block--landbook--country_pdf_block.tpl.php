7d5943a5b472e4469ae75f3394ef343309a4942f
SergeyDotsenko <sergdots@ukr.net>
2016-09-14 18:21:16 +0300

    The "give us your feedback" button sends to a broken page

--- modules/landbook/templates/block--landbook--country_pdf_block.tpl.php
+++ modules/landbook/templates/block--landbook--country_pdf_block.tpl.php
@@ -11,7 +11,12 @@
 <?php } ?>
                </div>
         <div class="col-sm-4 txt-c">
-          <a class="displayb" href="/contact-us">
+          <?php 
+          global $language;
+          $translations = translation_path_get_translations("node/30375");
+          $url=drupal_get_path_alias($translations[$language->language]);
+          ?>
+          <a class="displayb" href="<?php print '/'.$language->language.'/'.$url; ?>">
           <img class="c-obj" alt="Feedback" src="<?php print '/'.$theme_path.'/images/ico-feedback.svg';?>">
           <span class="displayb txt-m m-s-top fos" data-localize="global.feedback">Give us your feedback</span>
           </a>
