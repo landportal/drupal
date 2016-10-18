8097692deb67e1735dd46c14c2c997acf39e60d5
Anton Zuev <zuev13@mail.ru>
2016-10-18 13:38:48 +0300

    Fixed horizontal scroll for "ranking" block

--- modules/landbook/modules/view-coda/block--landbook-view-coda--landbook-vis-ranking.tpl.php
+++ modules/landbook/modules/view-coda/block--landbook-view-coda--landbook-vis-ranking.tpl.php
@@ -1,42 +1,41 @@
-    <section id="ranking" class="container">
+<section id="ranking" class="container">
 <?php if(!empty($block->subject)): ?>
-      <header class="row">
-        <div class="col-xs-12">
-          <h2><?php print $block->subject; ?></h2>
-        </div>
-      </header>
+  <header class="row">
+    <div class="col-xs-12">
+      <h2><?php print $block->subject; ?></h2>
+    </div>
+  </header>
 <?php endif;?>
-      <nav class="row">
-        <form>
-          <div class="form-group col-xs-4 col-xs-push-3">
-            <select name="indicator" class="form-control">
-              <option data-localize="inputs.indicators">Select an indicator...</option>
-            </select>
-          </div>
-          <div class="form-group col-xs-2 col-xs-push-3">
-            <select name="year" class="form-control" disabled="disabled">
-              <option data-localize="inputs.years">Year...</option>
-            </select>
-          </div>
-        </form>
-      </nav>
-      <div class="row metadata">
-        <div class="col-md-offset-2 col-md-8 text-center h2 visible-print-block">
-          <a><span name="indicator"></span></a>
-          <span name="year" class="h3"></span>
-          </div>
-        <div class="col-md-offset-2 col-md-8 text-center h3">
-          <a><span name="dataset"></span></a> -
-          <a><span name="source"></span></a>
-        </div>
-        <div class="col-md-offset-2 col-md-8 text-center">
-          <p><span name="description"></span></p>
-        </div>
-        </div>
+  <nav class="row">
+    <form>
+      <div class="form-group col-xs-4 col-xs-push-3">
+        <select name="indicator" class="form-control">
+          <option data-localize="inputs.indicators">Select an indicator...</option>
+        </select>
       </div>
-      <div class="row">
-        <div class="col-xs-10 col-xs-push-1 col-sm-8 col-sm-push-2 col-lg-6 col-lg-push-3">
-          <ul id="ranking-wrapper" class="ranking list-unstyled"></ul>
-        </div>
+      <div class="form-group col-xs-2 col-xs-push-3">
+        <select name="year" class="form-control" disabled="disabled">
+          <option data-localize="inputs.years">Year...</option>
+        </select>
       </div>
-    </section>
+    </form>
+  </nav>
+  <div class="row metadata">
+    <div class="col-md-offset-2 col-md-8 text-center h2 visible-print-block">
+      <a><span name="indicator"></span></a>
+      <span name="year" class="h3"></span>
+    </div>
+    <div class="col-md-offset-2 col-md-8 text-center h3">
+      <a><span name="dataset"></span></a> -
+      <a><span name="source"></span></a>
+    </div>
+    <div class="col-md-offset-2 col-md-8 text-center">
+      <p><span name="description"></span></p>
+    </div>
+  </div>
+  <div class="row">
+    <div class="col-xs-10 col-xs-push-1 col-sm-8 col-sm-push-2 col-lg-6 col-lg-push-3">
+      <ul id="ranking-wrapper" class="ranking list-unstyled"></ul>
+    </div>
+  </div>
+</section>
